985d90b5b67db96e47ba2092edae3148b63193f0
kunal351411 <kanotrakunal030501@gmail.com>
2023-08-18 00:31:01 +0530

    fix: add dependency array

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export const App = () => {
   useEffect(() => {
     setSavedTablesState(savedTables);
     setSavedQueriesState(savedQueries);
-  }, []);
+  }, [setSavedQueriesState, setSavedTablesState]);
 
   return (
     <>
