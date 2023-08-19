7e0fb4b4a89f1d27d280a343cbd6c80e22562fd2
kunal351411 <kanotrakunal030501@gmail.com>
2023-08-19 14:01:11 +0530

    improve FCP

--- src/components/output/Output.jsx
+++ src/components/output/Output.jsx
@@ -1,6 +1,6 @@
 import { lazy, Suspense } from "react";
 import { useRecoilValue } from "recoil";
-import { FaWarning, FaDownload} from 'react-icons/fa';
+import { FaDownload, FaThumbtack } from "react-icons/fa";
 import toast from "react-hot-toast";
 import { queryResultState } from "../../state/atoms";
 import { rowsCountState } from "../../state/selectors";
@@ -12,9 +12,11 @@ import Loader from "../loader/Loader";
 import "./Output.css";
 
 const Table = lazy(() => import("../table/Table"));
-const CSVLink = lazy(() => import('react-csv').then((module) => {
-  return { default : module.CSVLink}
-}))
+const CSVLink = lazy(() =>
+  import("react-csv").then((module) => {
+    return { default: module.CSVLink };
+  })
+);
 
 const Output = () => {
   const resultData = useRecoilValue(queryResultState);
@@ -31,45 +33,45 @@ const Output = () => {
   return (
     <>
       <Suspense fallback={<Loader />}>
-      <div className="output-header">
-        <div className="section-header">
-          <h3>Output</h3>
-        </div>
-        
-        {resultData.length > 0 && (
-          <div className="btn-container">
-            <div className="row-count">
-              <h5>{`Fetched ${rowsCount} records in 0.3 s`}</h5>
-            </div>
-            <CSVLink
-              data={resultData}
-              filename={"result.csv"}
-              headers={getHeaders()}
-              onClick={() => toast.success("CSV exported successfully")}
-            >
-              <div className="btn">
-                <button type="button" className="export">
-                  Export&emsp;<FaDownload/>
-                </button>
-              </div>
-            </CSVLink>
+        <div className="output-header">
+          <div className="section-header">
+            <h3>Output</h3>
           </div>
-        )}
-      </div>
-      <div className="table-container">
-        
+
+          {resultData.length > 0 && (
+            <div className="btn-container">
+              <div className="row-count">
+                <h5>{`Fetched ${rowsCount} records in 0.3 s`}</h5>
+              </div>
+              <CSVLink
+                data={resultData}
+                filename={"result.csv"}
+                headers={getHeaders()}
+                onClick={() => toast.success("CSV exported successfully")}
+              >
+                <div className="btn">
+                  <button type="button" className="export">
+                    Export&emsp;
+                    <FaDownload />
+                  </button>
+                </div>
+              </CSVLink>
+            </div>
+          )}
+        </div>
+        <div className="table-container">
           {!resultData.length ? (
-            <div className="no-result">
+            <div style={{color: '#475460', fontWeight: 600}} className="no-result">
               <h3>
-                <i className="fa fa-warning"></i>&emsp;
-                {`Run Some Query to See Output !!!`}
+                <FaThumbtack />
+                &emsp;
+                Output will be displayed here!!!
               </h3>
             </div>
           ) : (
             <Table />
           )}
-        
-      </div>
+        </div>
       </Suspense>
     </>
   );
